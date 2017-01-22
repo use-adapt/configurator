b4137c369622806b0514404e21a8acef72e86c95
Zack Marvel <zpmarvel@gmail.com>
2017-01-21 23:48:54 -0500

    actually started this project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Item, Button, Header, Icon, Container, Segment, Grid, Checkbox, List } from 'semantic-ui-react'
+import data from '../data/data.json';
 
-class App extends Component {
+class Requirement extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Checkbox
+        key={this.props.name}
+        name={this.props.name}
+        label={<label name={this.props.name}>{this.props.name}</label>}
+        onChange={this.props.onButtonClick}
+        checked={this.props.selectStatus[this.props.name]}
+        />
+    );
+  }
+}
+
+class RequirementCategory extends React.Component {
+  render() {
+    var elements = this.props.reqs.map((req) => (
+      <List.Item key={req}>
+        <Requirement
+        name={req}
+        onButtonClick={this.props.onButtonClick}
+        selectStatus={this.props.selectStatus}
+        />
+      </List.Item>
+    ));
+    return (
+      <Item>
+        <Item.Content>
+        <Header textAlign="center">{this.props.name}</Header>
+        <List>
+          {elements}
+        </List>
+        </Item.Content>
+      </Item>
+
+    );
+  }
+}
+
+class RequirementSection extends React.Component {
+  render() {
+    const children = Object.keys(this.props.requirements).map((name) => {
+      const reqs = this.props.requirements[name];
+      return (
+        <div key={name}>
+          <RequirementCategory
+            name={name}
+            reqs={reqs}
+            selectStatus={this.props.selectStatus}
+            onButtonClick={this.props.onButtonClick}
+            />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      )
+    });
+    return (
+      <div>
+        <Header as="h2" textAlign="center">
+          Requirements
+        </Header>
+      <Segment color="teal">
+        <Grid padded='vertically' divided columns='equal' stackable stretched doubling verticalAlign='middle'>
+          {children}
+        </Grid>
+      </Segment>
       </div>
     );
   }
 }
 
-export default App;
+class Configurator extends Component {
+  render() {
+    const categories = data.project.map((c) =>
+      <RequirementSection category={c} />
+    );
+ 
+    return (
+      <div className="Configurator">
+            {categories}
+      </div>
+    );
+  }
+}
+
+export default Configurator;
